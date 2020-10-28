7dd376b92f34d76de0c3e5420f0d8fecf2528ebf
unknown <bart.onstee@incentro.com>
2020-10-28 23:30:50 +0100

    Initial commit.

---
+++ src/CustomGroupbox.tsx
@@ -0,0 +1,80 @@
+import { Component, ReactNode, createElement } from "react";
+import { TextStyle, ViewStyle, View, TouchableOpacity } from "react-native";
+import { DynamicValue, NativeIcon, ValueStatus } from "mendix";
+import { Icon } from 'mendix/components/native/Icon';
+
+import { Style, mergeNativeStyles } from '@mendix/pluggable-widgets-tools';
+
+import { CustomGroupboxProps } from '../typings/CustomGroupboxProps';
+
+export interface CustomStyle extends Style {
+  container: ViewStyle;
+  label: TextStyle;
+  innerContainer: ViewStyle;
+  selectedContainer: ViewStyle;
+  divider: ViewStyle;
+  icon: ViewStyle;
+}
+
+const defaultStyle: CustomStyle = {
+  container: {},
+  label: {},
+  innerContainer: {},
+  selectedContainer: {},
+  divider: {
+    backgroundColor: '#A2A2A2',
+    height: 1,
+  },
+  icon: {
+    alignSelf: 'flex-end'
+  }
+};
+
+interface State {
+  showContent: boolean;
+}
+
+const defaultCollapseIconGlyph = "glyphicon-minus";
+const defaultExpandIconGlyph = "glyphicon-plus";
+
+export class CustomGroupbox extends Component<CustomGroupboxProps<CustomStyle>, State> {
+  constructor(props: CustomGroupboxProps<CustomStyle>){
+    super(props)
+    this.toggleContent = this.toggleContent.bind(this)
+    this.state = { showContent: false, }
+  }
+
+  private readonly styles = mergeNativeStyles(defaultStyle, this.props.style);
+    
+  render(): ReactNode {
+    const icons = {
+      collapseIcon: this.renderIcon(defaultCollapseIconGlyph, this.props.collapseIcon),
+      expandIcon: this.renderIcon(defaultExpandIconGlyph, this.props.expandIcon),
+    };
+      
+    return (
+      <View style={this.state.showContent ? this.styles.selectedContainer : this.styles.container}>
+        <View style={this.styles.icon}>{this.state.showContent ? icons.collapseIcon : icons.expandIcon}</View>
+
+        <TouchableOpacity onPress={this.toggleContent}>{this.props.header}</TouchableOpacity>
+        
+        {this.state.showContent && <View style={this.styles.innerContainer}>{this.props.content}</View>}
+
+        {this.props.showDivider && <View style={this.styles.divider}></View>}
+      </View>
+    );
+  }
+
+  toggleContent() {
+    this.setState({showContent: !this.state.showContent})
+  }
+
+  private renderIcon = (glyph: string, toBeRenderedIcon?: DynamicValue<NativeIcon>) => {
+    const nativeIcon: NativeIcon =
+      toBeRenderedIcon && toBeRenderedIcon.status === ValueStatus.Available
+        ? toBeRenderedIcon.value
+        : { type: "glyph", iconClass: glyph };
+
+    return <Icon icon={nativeIcon} />;
+  };
+}
---
+++ typings/CustomGroupboxProps.d.ts
@@ -0,0 +1,27 @@
+/**
+ * This file was generated from CustomGroupbox.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+import { ComponentType, ReactNode } from "react";
+import { DynamicValue, NativeIcon } from "mendix";
+
+export interface CustomGroupboxProps<Style> {
+    name: string;
+    style: Style[];
+    header: ReactNode;
+    content?: ReactNode;
+    showDivider: boolean;
+    expandIcon?: DynamicValue<NativeIcon>;
+    collapseIcon?: DynamicValue<NativeIcon>;
+}
+
+export interface CustomGroupboxPreviewProps {
+    class: string;
+    style: string;
+    header: { widgetCount: number; renderer: ComponentType };
+    content: { widgetCount: number; renderer: ComponentType };
+    showDivider: boolean;
+    expandIcon: { type: "glyph"; iconClass: string; } | { type: "image"; imageUrl: string; } | null;
+    collapseIcon: { type: "glyph"; iconClass: string; } | { type: "image"; imageUrl: string; } | null;
+}
